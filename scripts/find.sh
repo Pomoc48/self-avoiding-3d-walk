1827bc74e6e6a19268aa1902f4f656d8d91d9cb1
Pomoc48 <lukawski.mikolaj@gmail.com>
2022-09-24 13:48:22 +0200

    Add README

--- scripts/find.sh
+++ scripts/find.sh
@@ -1,1 +1,2 @@
-for i in `seq 1 1 12`; do ./test > output.txt & done ; watch -gn 10 grep "tries" output.txt; killall ./calc;
+for i in `seq 1 1 12`; do ./calc > output.txt & done ; watch -gn 10 grep "tries" output.txt; killall ./calc;
+#                 ^^ numer of cpu cores you want to use
